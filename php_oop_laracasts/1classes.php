5ce3616708f72b147238b695b5730b83e0c7dcac
Avutzhan Dautov <dautov92@list.ru>
2020-10-14 11:56:35 +0600

    classes

---
+++ php_oop_laracasts/1classes.php
@@ -0,0 +1,20 @@
+<?php
+// Let's begin with an introduction to classes in PHP.
+// I like to think of a class as a blueprint or template that defines the overall
+// structure and behavior for some concept in your codebase.
+class User
+{
+
+}
+
+class AchievementBadge
+{
+    public $title;
+    public $description;
+    public $points;
+
+    public function awardTo($user)
+    {
+
+    }
+}
