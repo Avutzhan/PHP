88b2c556c7f467d6bb33ee90c2390dc1a26dcf9c
Avutzhan Dautov <dautov92@list.ru>
2020-10-15 16:46:07 +0600

    youtube anchor

--- SOLID/S/index.php
+++ SOLID/S/index.php
@@ -1,5 +1,6 @@
 <?php
 //Pseudo code for examples
+// https://www.youtube.com/watch?v=EaF1fHQIe0Y&list=PLoonZ8wII66jOXzIvQCtKosCstjGPL0lg&index=1
 
 $logger = new Logger();
 $product = new Product($logger);
