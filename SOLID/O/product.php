5ceb85b788a09662f027cebf8dde37e69aafcff1
Avutzhan Dautov <dautov92@list.ru>
2020-10-15 15:52:36 +0600

    code without OCP

---
+++ SOLID/O/product.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product {
+
+    private $logger;
+
+    public function __construct(Logger $logger) {
+        $this->logger = $logger;
+    }
+
+    public function setPrice() {
+        try {
+            //save price in db
+        } catch (DBException $e) {
+            $this->logger->log($e->getMessage());
+        }
+    }
+}
