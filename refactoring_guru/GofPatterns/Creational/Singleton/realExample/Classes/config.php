2737b3b0261075f28739b4cf4df542ab5ee77885
Avutzhan Dautov <dautov92@list.ru>
2020-12-13 19:38:23 +0600

    singleton real example split

---
+++ refactoring_guru/GofPatterns/Creational/Singleton/realExample/Classes/config.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("Classes/Singleton.php");
+
+class Config extends Singleton
+{
+    private $hashmap = [];
+
+    public function getValue(string $key): string
+    {
+        return $this->hashmap[$key];
+    }
+
+    public function setValue(string $key, string $value): void
+    {
+        $this->hashmap[$key] = $value;
+    }
+}
