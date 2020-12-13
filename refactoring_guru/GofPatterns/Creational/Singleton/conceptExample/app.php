22632c4059533cb7ad80b4536ba5421ff066b792
Avutzhan Dautov <dautov92@list.ru>
2020-12-13 19:28:28 +0600

    singleton concept example split

---
+++ refactoring_guru/GofPatterns/Creational/Singleton/conceptExample/app.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("Classes/Singleton.php");
+
+function clientCode()
+{
+    $s1 = Singleton::getInstance();
+    $s2 = Singleton::getInstance();
+    if ($s1 === $s2) {
+        echo "Singleton works, both variables contain the same instance.";
+    } else {
+        echo "Singleton failed, variables contain different instances.";
+    }
+}
