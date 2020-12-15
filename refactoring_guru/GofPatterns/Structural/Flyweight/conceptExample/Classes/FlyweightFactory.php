c74cbc1178064eeae44f21be1d18fec6dc53be42
Avutzhan Dautov <dautov92@list.ru>
2020-12-15 15:20:07 +0600

    flyweight concept example split

---
+++ refactoring_guru/GofPatterns/Structural/Flyweight/conceptExample/Classes/FlyweightFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+class FlyweightFactory
+{
+    private $flyweights = [];
+
+    public function __construct(array $initialFlyweights)
+    {
+        foreach ($initialFlyweights as $state) {
+            $this->flyweights[$this->getKey($state)] = new Flyweight($state);
+        }
+    }
+
+    private function getKey(array $state): string
+    {
+        ksort($state);
+
+        return implode("_", $state);
+    }
+
+    public function getFlyweight(array $sharedState): Flyweight
+    {
+        $key = $this->getKey($sharedState);
+
+        if (!isset($this->flyweights[$key])) {
+            echo "FlyweightFactory: Can't find a flyweight, creating new one.\n";
+            $this->flyweights[$key] = new Flyweight($sharedState);
+        } else {
+            echo "FlyweightFactory: Reusing existing flyweight.\n";
+        }
+
+        return $this->flyweights[$key];
+    }
+
+    public function listFlyweights(): void
+    {
+        $count = count($this->flyweights);
+        echo "\nFlyweightFactory: I have $count flyweights:\n";
+        foreach ($this->flyweights as $key => $flyweight) {
+            echo $key . "\n";
+        }
+    }
+}
---
+++ refactoring_guru/GofPatterns/Structural/Flyweight/conceptExample/app.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once("Classes/FlyweightFactory.php");
+include_once("Classes/Flyweight.php");
+
+$factory = new FlyweightFactory([
+    ["Chevrolet", "Camaro2018", "pink"],
+    ["Mercedes Benz", "C300", "black"],
+    ["Mercedes Benz", "C500", "red"],
+    ["BMW", "M5", "red"],
+    ["BMW", "X6", "white"],
+    // ...
+]);
+$factory->listFlyweights();
+
+// ...
+
+function addCarToPoliceDatabase(
+    FlyweightFactory $ff, $plates, $owner,
+    $brand, $model, $color
+) {
+    echo "\nClient: Adding a car to database.\n";
+    $flyweight = $ff->getFlyweight([$brand, $model, $color]);
+
+    $flyweight->operation([$plates, $owner]);
+}
+
+addCarToPoliceDatabase($factory,
+    "CL234IR",
+    "James Doe",
+    "BMW",
+    "M5",
+    "red"
+);
+
+addCarToPoliceDatabase($factory,
+    "CL234IR",
+    "James Doe",
+    "BMW",
+    "X1",
+    "red"
+);
+
+$factory->listFlyweights();
