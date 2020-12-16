d9b95e9297dc657daf429e4ee9bd73e5cef7265e
Avutzhan Dautov <dautov92@list.ru>
2020-12-16 16:33:06 +0600

    iterator concept example split

---
+++ refactoring_guru/GofPatterns/Behavioral/Iterator/conceptExample/main.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once("Classes/WordsCollection.php");
+
+$collection = new WordsCollection();
+$collection->addItem("First");
+$collection->addItem("Second");
+$collection->addItem("Third");
+
+echo "Straight traversal:\n";
+foreach ($collection->getIterator() as $item) {
+    echo $item . "\n";
+}
+
+echo "\n";
+echo "Reverse traversal:\n";
+foreach ($collection->getReverseIterator() as $item) {
+    echo $item . "\n";
+}
